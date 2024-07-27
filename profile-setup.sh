8c5e506b98894d811a9ebecc56bc6112e78fa11e
JaehongDev <println@kakao.com>
2024-07-27 22:10:00 +0900

    chore: add profile script

---
+++ profile-setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+msg_write(){ 
+	echo ""
+	echo "==========================================="
+	echo "$1"
+	echo "==========================================="
+	echo ""
+}
+
+
+
+read -p "input session timeout: " TIME_OUT
+
+if ! echo "$TIME_OUT" | grep -qE '^[0-9]+$'; then
+	echo "시간 입력은 숫자만 가능해요"
+	exit 1
+fi
+
+read -p "server name: " USERNAME
+
+
+PROFILE_PATH="~/.profile"
+BASHRC_PATH="~/.bashrc"
+
+
+
+echo 'HISTTIMEFORMAT="%F %T -- "' >> "$PROFILE_PATH"
+echo 'export HISTTIMEFORMAT' >> "$PROFILE_PATH"
+echo "export TMOUT=$TIME_OUT" >> "$PROFILE_PATH"
+
+source "$PROFILE_PATH"
+
+msg_write "write session timeout"
+
+echo "PS1='[\e[1;31m$USERNAME\e[0m][\e[1;32m\t\e[0m][\e[1;33m\u\e[0m@\e[1;36m\h\e[0m \w] \n\$ \[\033[00m\]'" >> "$BASHRC_PATH"
+
+echo "tty=`tty | awk -F"/dev/" '{print $2}'`" >> "$BASHRC_PATH"
+echo "IP=`w | grep "$tty" | awk '{print $3}'`" >> "$BASHRC_PATH"
+echo "export PROMPT_COMMAND='logger -p local0.debug "[USER]$(whoami) [IP]$IP [PID]$$ [PWD]`pwd` [COMMAND] $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" )"'" >> "$BASHRC_PATH"
+
+source "$BASHRC_PATH"
+
+
+
+
